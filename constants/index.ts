523da545e920007e0016933b8eb260c33b4eda65
Ruhan Khandakar <khandakarruhan@gmail.com>
2022-11-19 12:11:07 +0530

    whitelist frontend

---
+++ constants/index.ts
@@ -0,0 +1,68 @@
+export const WHITELIST_CONTRACT_ADDRESS =
+  process.env.NEXT_PUBLIC_WHITELIST_CONTRACT_ADDRESS;
+
+export const ABI = [
+  {
+    inputs: [
+      {
+        internalType: 'uint8',
+        name: '_maxWhiteListedAddresses',
+        type: 'uint8',
+      },
+    ],
+    stateMutability: 'nonpayable',
+    type: 'constructor',
+  },
+  {
+    inputs: [],
+    name: 'addAddressToWhiteList',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'maxWhiteListedAddresses',
+    outputs: [
+      {
+        internalType: 'uint8',
+        name: '',
+        type: 'uint8',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'numAddressesWhitelisted',
+    outputs: [
+      {
+        internalType: 'uint8',
+        name: '',
+        type: 'uint8',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address',
+      },
+    ],
+    name: 'whiteListedAddresses',
+    outputs: [
+      {
+        internalType: 'bool',
+        name: '',
+        type: 'bool',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+];
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,71 +1,211 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Web3Modal, { ICoreOptions } from 'web3modal';
+import { providers, Contract } from 'ethers';
+import { useEffect, useRef, useState } from 'react';
 
-export default function Home() {
+import { WHITELIST_CONTRACT_ADDRESS, ABI } from '../constants';
+
+import styles from '../styles/Home.module.css';
+
+const Home = () => {
+  const [numberOfWhitelisted, setNumberOfWhitelisted] = useState(0);
+  const [walletConnected, setWalletConnected] = useState(false);
+  const [loading, setLoading] = useState(false);
+  // joinedWhitelist keeps track of whether the current metamask address has joined the Whitelist or not
+  const [joinedWhitelist, setJoinedWhitelist] = useState(false);
+
+  // Create a reference to the Web3 Modal (used for connecting Metamask) which persists as long as the page is open
+  const web3ModalRef = useRef<any | undefined>();
+
+  /* 
+    Returns a Provider or Signer object representing the Ethereum RPC with or without the
+    Signing capabilities of metamask attached
+
+  */
+  const getProviderOrSigner = async (needSigner = false) => {
+    // Connect to Metamask
+    // Since we store `web3Modal` as a reference, we need to access the `current` value to get access to the underlying object
+    const provider = await web3ModalRef?.current?.connect();
+    const web3Provider = new providers.Web3Provider(provider);
+
+    // If user is not connected to Goerli network, let them know and throw an error
+    const { chainId } = await web3Provider.getNetwork();
+    if (chainId !== 5) {
+      window.alert('Change the network to Goerli');
+      throw new Error('Change network to Goerli');
+    }
+
+    if (needSigner) {
+      const signer = web3Provider.getSigner();
+      return signer;
+    }
+    return web3Provider;
+  };
+
+  /**
+   * checkIfAddressInWhitelist: Checks if the address is in whitelist
+   */
+  const checkIfAddressInWhitelist = async () => {
+    try {
+      // we will need the signer later to get the user's address
+      // Even though it is a read transaction, since Singer are just special kind of Providers,
+      // We can use it in it's place
+      const signer = (await getProviderOrSigner(true)) as any;
+      const whitelistContract = new Contract(
+        WHITELIST_CONTRACT_ADDRESS,
+        ABI,
+        signer
+      );
+
+      // Get the address associated to the signer which is connected to Metamask
+      const address = await signer.getAddress();
+
+      // Call the whitelistedAddress from the contract
+      const _joinedWhitelist = await whitelistContract.whiteListedAddresses(
+        address
+      );
+      setJoinedWhitelist(_joinedWhitelist);
+    } catch (error) {
+      console.error('checkIfAddressInWhitelist', error);
+    }
+  };
+
+  /**
+   * getNumberOfWhitelisted:  gets the number of whitelisted addresses
+   */
+  const getNumberOfWhitelisted = async () => {
+    try {
+      // Get the provider from web3Modal, which in our case is MetaMask
+      // No need for the Signer here, as we are only reading state from the blockchain
+      const provider = await getProviderOrSigner();
+      // We connect to the Contract using a Provider, so we will only
+      // have read-only access to the Contract
+      const whitelistContract = new Contract(
+        WHITELIST_CONTRACT_ADDRESS,
+        ABI,
+        provider
+      );
+      // call the numAddressesWhitelisted from the contract
+      const _numberOfWhitelisted =
+        await whitelistContract.numAddressesWhitelisted();
+      setNumberOfWhitelisted(_numberOfWhitelisted);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const addAddressToWhitelist = async () => {
+    try {
+      // we need a Signer here since this is a `write` operation
+      const signer = await getProviderOrSigner(true);
+
+      //  Create a new instance of the Contract with a signer, which allows
+
+      const whitelistContract = new Contract(
+        WHITELIST_CONTRACT_ADDRESS,
+        ABI,
+        signer
+      );
+
+      // call the addAddressToWhiteList from the contract
+      const tx = await whitelistContract.addAddressToWhiteList();
+      setLoading(true);
+
+      // await for the transaction to get mined
+      await tx.wait();
+      setLoading(false);
+
+      // get the updated number of address in whitelist
+      await getNumberOfWhitelisted();
+      setJoinedWhitelist(true);
+    } catch (error) {
+      console.error('addAddressToWhitelist', error);
+    }
+  };
+
+  /* 
+  connectWallet: Connect to Metamask Wallet
+  */
+  const connectWallet = async () => {
+    try {
+      await getProviderOrSigner();
+      setWalletConnected(true);
+
+      checkIfAddressInWhitelist();
+      getNumberOfWhitelisted();
+    } catch (error) {
+      console.error('connectWallet', error);
+    }
+  };
+
+  const renderButton = () => {
+    if (walletConnected) {
+      if (joinedWhitelist) {
+        return (
+          <div className={styles.description}>
+            Thanks for joining the Whitelist!
+          </div>
+        );
+      } else if (loading) {
+        return <button className={styles.button}>Loading...</button>;
+      } else {
+        return (
+          <button onClick={addAddressToWhitelist} className={styles.button}>
+            Join the Whitelist
+          </button>
+        );
+      }
+    } else {
+      return (
+        <button onClick={connectWallet} className={styles.button}>
+          Connect your wallet
+        </button>
+      );
+    }
+  };
+
+  useEffect(() => {
+    if (!walletConnected) {
+      // Assign the Web3Modal class to the reference object by settings it's 'current' value
+      // The 'current' value is persisted throughout as long as this is open
+      web3ModalRef.current = new Web3Modal({
+        network: 'goerli',
+        providerOptions: {},
+        disableInjectedProvider: false,
+      });
+      connectWallet();
+    }
+  }, [walletConnected]);
+
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Generated by create next app" />
+        <title>Whitelist Dapp</title>
+        <meta name="description" content="Whitelist-Dapp" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className={styles.main}>
+        <div>
+          <h1 className={styles.title}>Welcome to Crypto Devs!</h1>
+          <div className={styles.description}>
+            Its an NFT collection for developers in Crypto.
+          </div>
+          <div className={styles.description}>
+            {numberOfWhitelisted} have already joined the Whitelist
+          </div>
+          {renderButton()}
         </div>
-      </main>
+        <div>
+          <img className={styles.image} src="./crypto-devs.svg" />
+        </div>
+      </div>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+        Made with &#10084; by Crypto Devs
       </footer>
     </div>
-  )
-}
+  );
+};
+
+export default Home;
